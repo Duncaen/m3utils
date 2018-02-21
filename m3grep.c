3c6ec47e66648d62adcb5edaada73b8a794b9cac
Duncaen <mail@duncano.de>
2018-02-21 15:42:28 +0100

    m3grep: add more flags

--- m3grep.c
+++ m3grep.c
@@ -8,12 +8,18 @@
 
 enum {
 	Tall = 1,
-	Tartist,
 	Talbum,
+	Tartist,
 	Ttitle,
 };
 
+static int cflag = 0;
 static int iflag = 0;
+static int mflag = 0;
+static int oflag = 0;
+static int pflag = 0;
+static int qflag = 0;
+static int vflag = 0;
 
 static long matches = 0;
 static int tag = Tall;
@@ -21,46 +27,79 @@ static int tag = Tall;
 static regex_t pattern;
 
 int
-match(char *s)
+match(char *file, char *tag, char *s)
 {
-	regmatch_t pmatch;
-	while (*s && regexec(&pattern, s, 1, &pmatch, 0) == 0) {
+	if (oflag && !cflag && !qflag && !vflag) {
+		regmatch_t pmatch;
+		int len, matched = 0;
+		while (*s && regexec(&pattern, s, 1, &pmatch, 0) == 0) {
+			s += pmatch.rm_so;
+			if (!(len = pmatch.rm_eo-pmatch.rm_so)) {
+				s += 1;
+				continue;
+			}
+			if (pflag)
+				printf("%s: %s: ", file, tag);
+			printf("%.*s\n", len, s);
+			s += len;
+			matched++;
+		}
+		return (matched && matches++);
+	} else if (vflag ^ (regexec(&pattern, s, 0, 0, 0) == 0)) {
+		if (qflag)
+			exit(0);
 		matches++;
+		if (!cflag) {
+			printf("%s", file);
+			if (pflag && !vflag)
+				printf(": %s: %s", tag, s);
+			putchar('\n');
+		}
+		if (mflag && matches >= mflag)
+			exit(0);
 		return 1;
 	}
 	return 0;
 }
 
 void
-oneline(char *f)
+grep(char *f)
 {
 	struct tags t;
-	char *s;
 
 	if (m3tags(f, &t) == -1)
 		return;
 
 	switch (tag) {
-	case Tartist: s = t.artist; break;
-	case Talbum: s = t.album; break;
-	case Ttitle: s = t.title; break;
+	case Talbum: match(f, "album", t.album); break;
+	case Tartist: match(f, "artist", t.artist); break;
+	case Ttitle: match(f, "title", t.title); break;
 	case Tall:
-		if (match(t.artist) || match(t.album) || match(t.title))
-			printf("%s\n", f);
-		return;
+		if (cflag) {
+			(void)(match(f, "album", t.album) ||
+			    match(f, "album", t.album) ||
+			    match(f, "album", t.album));
+			return;
+		}
+		match(f, "album", t.album);
+		match(f, "artist", t.artist);
+		match(f, "title", t.title);
 	}
-
-	if (match(s))
-		printf("%s\n", f);
 }
 
 int
 main(int argc, char *argv[])
 {
 	int c, i;
-	while ((c = getopt(argc, argv, "i")) != -1)
+	while ((c = getopt(argc, argv, "cim:opqv")) != -1)
 		switch (c) {
+		case 'c': cflag = 1;
 		case 'i': iflag = REG_ICASE; break;
+		case 'm': mflag = atol(optarg); break;
+		case 'o': oflag = 1; break;
+		case 'p': pflag = 1; break;
+		case 'q': qflag = 1; break;
+		case 'v': vflag = 1; break;
 		default:
 usage:
 			fprintf(stderr,
@@ -93,9 +132,12 @@ usage:
 	}
 
 	if (argc == optind && isatty(0))
-		m3u_loop(argc-optind, argv+optind, oneline);
+		m3u_loop(argc-optind, argv+optind, grep);
 	else
-		m3u_loop(argc-optind, argv+optind, oneline);
+		m3u_loop(argc-optind, argv+optind, grep);
+
+	if (cflag)
+		printf("%ld\n", matches);
 
 	return !matches;
 }
